71ec1c28ef1374eb8596d950826e14a2ad7a6290
Zheng20010712 <Zheng20010712@163.com>
2023-05-14 19:37:36 +0800

    feat: replaceFetch

---
+++ src/observe/common/fetch.ts
@@ -0,0 +1,48 @@
+import {_global, _support} from "../../utils/global"
+import { EVENT_TYPES, ACTION_TYPES, STATUS_CODE } from "../action/types"
+import { replaceAop } from "./replace"
+
+/**
+ * 待debug
+ */
+const fetchReaplace = function () {
+    const fetchReplaceFn = function(originalFetch: Function) {
+        return function (url: string, config: Partial<Request> = {}) {
+        //记录开始时间
+        const startTime = Date.now()
+        // 获取配置的headers
+        const headers = new Headers(config.headers || {});
+        Object.assign(headers, {
+          setRequestHeader: headers.set,
+        });
+        config = Object.assign({}, config, headers);
+        // 调用原始的fetch方法
+          return originalFetch.apply(_global, [url,config]).then(() => {
+              const curTime = Date.now()
+              _support.actionStore.push({
+                type: EVENT_TYPES.FETCH, // 事件类型
+                category: ACTION_TYPES.HTTP, // 用户行为类型
+                status: STATUS_CODE.OK, // 行为状态
+                time: curTime, // 发生时间
+                  data: {
+                      startTime: startTime,
+                      endTime: curTime
+                }
+            })
+          }, () => {
+            const curTime = Date.now()
+            _support.actionStore.push({
+                type: EVENT_TYPES.FETCH, // 事件类型
+                category: ACTION_TYPES.HTTP, // 用户行为类型
+                status: STATUS_CODE.ERROR,// 行为状态
+                time: curTime, // 发生时间
+                  data: {
+                      startTime: startTime,
+                      endTime: curTime
+                }
+            })
+        })
+        }
+      }
+    replaceAop(window,'fetch',fetchReplaceFn)
+}
---
+++ src/observe/common/xhr.ts
@@ -0,0 +1,17 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: ZhengXiaoRui
+ * @email: zheng20010712@163.com
+ * @Date: 2023-05-14 19:13:09
+ * @LastEditors: ZhengXiaoRui
+ * @LastEditTime: 2023-05-14 19:37:14
+ */
+// const originalOpen = XMLHttpRequest.prototype.open
+
+// XMLHttpRequest.prototype.open = function(method, url, async) {
+//   // 在这里添加你的代码
+//   console.log(`XHR request to ${url}`)
+//   // 调用原始的open方法
+//   originalOpen.apply(this, arguments)
+// }
